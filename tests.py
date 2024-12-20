c4b7160a9261369fb6622da97d4807dc6caa5ac3
Joel Dankert <joel.dankert@gmail.com>
2024-12-20 23:26:57 +0100

    added createvector

--- tests.py
+++ tests.py
@@ -9,6 +9,8 @@ class MainScene(Scene):
         circle = Circle()
         circle.set_fill(opacity=0)
         circle.set_stroke(RED)
+        
+        
         a.stretch_to_fit_height(circle.height)
         a.stretch_to_fit_width(circle.width*3)
 
@@ -18,6 +20,18 @@ class MainScene(Scene):
         self.add(circle)
         self.play(Transform(circle,graph),run_time=1)
         self.play(FadeOut(graph))
+
+        vec = a.get_vector([0,1])
+        self.play(GrowArrow(vec))
+
+    def create_vector(start_coords, end_coords, axes):
+        start_point = axes.c2p(*start_coords)
+        end_point = axes.c2p(*end_coords)
+        direction = end_point - start_point
+        return Vector(direction).shift(start_point)
+
+
+
         
 
     def func(self,x):
