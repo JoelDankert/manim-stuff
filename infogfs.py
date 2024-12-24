1449d64a56c832621259117c4a8f9908b4a75754
Joel Dankert <joel.dankert@gmail.com>
2024-12-24 19:30:54 +0100

    error in color still

--- infogfs.py
+++ infogfs.py
@@ -44,17 +44,37 @@ def playsingleshuffle(self,oldbar,t):
 
     return oldbar
 
+def swaptwo(self, group, i1, i2,t,prevcol1,prevcol2):
+    # Get the bars to be swapped
+    bar1 = group[i1]
+    bar2 = group[i2]
+
+    # Calculate the shift vectors for the bars
+    shift1 = bar2.get_x() - bar1.get_x()
+    shift2 = bar1.get_x() - bar2.get_x()
+
+    # Create animations for the swap
+    self.play(bar1.animate.shift(RIGHT * shift1), bar2.animate.shift(RIGHT * shift2), run_time=t)
+
+    print(prevcol1,prevcol2)
+    group[i1].set_color(prevcol1)
+    group[i2].set_color(prevcol2)
+    group[i1], group[i2] = group[i2], group[i1]
+    
+    return group
+
+
 
 class MainScene(Scene):
     def construct(self):
-        bars = 50  # Example number of bars
-        colors = getcolorlist(bars)  # Get sorted colors
-        values = getlist(bars)  # Get sorted values
+        bars = 10
+        colors = getcolorlist(bars) 
+        values = getlist(bars)
 
         bar_chart = BarChart(
             values,
             bar_colors=colors,
-            y_range=[0, max(values) + 1, 10],  # Ticks every 1
+            y_range=[0, max(values) + 1, 10], 
             y_length=5
         )
         bars_group = bar_chart.bars
@@ -63,20 +83,38 @@ class MainScene(Scene):
         self.play(Create(bar_chart), run_time=1)
         bar_chart.bars.set_opacity(1)
         self.play(DrawBorderThenFill(bars_group),run_time=5)
+
         self.wait(3)
         
 
         for i in range(5):
-            bars_group = playsingleshuffle(self,bars_group,5-i)
+            bars_group = playsingleshuffle(self,bars_group,(5-i)/2)
 
-        bars_group = playsingleshuffle(self,bars_group,0.5)
+        self.wait(3)
+        
 
         sorted_bars = sorted(bars_group, key=lambda bar: bar.get_x())
         sorted_group = VGroup(*sorted_bars)
         self.remove(bars_group)
         self.add(sorted_group)
 
-        #for i in range(bars):
-        #    sorted_group[0].color = GREEN      sorted group is sorted by indecies and perfectly available
-    
+        swapped=True
+        while(swapped):
+            swapped=False
+            for i_bar in range(bars-1):
+                prevcol1=str(sorted_group[i_bar].get_color())
+                prevcol2=str(sorted_group[i_bar+1].get_color())
+                sorted_group[i_bar].set_color(GREEN)
+                sorted_group[i_bar+1].set_color(GREEN)
+                
+
+
+                if(sorted_group[i_bar].height>sorted_group[i_bar+1].height):
+                    sorted_group=swaptwo(self,sorted_group,i_bar,i_bar+1,0.2,prevcol1,prevcol2)
+                    
+                    swapped=True
+                else:
+                    self.wait(0.1)
+        
+        
         self.wait(3)
