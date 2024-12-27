1fed74ed05f776fe560b5ededfbc7981f5bdf735
Joel Dankert <joel.dankert@gmail.com>
2024-12-27 17:05:01 +0100

    text, intro and cleanup - safe build

--- infogfs.py
+++ infogfs.py
@@ -56,7 +56,6 @@ def swaptwo(self, group, i1, i2,t,prevcol1,prevcol2):
     # Create animations for the swap
     self.play(bar1.animate.shift(RIGHT * shift1), bar2.animate.shift(RIGHT * shift2), run_time=t)
 
-    print(prevcol1,prevcol2)
     group[i1].set_color(prevcol1)
     group[i2].set_color(prevcol2)
     group[i1], group[i2] = group[i2], group[i1]
@@ -64,57 +63,106 @@ def swaptwo(self, group, i1, i2,t,prevcol1,prevcol2):
     return group
 
 
+def bubblesort(self,bars=10,func=lambda x:5*0.96**x,explain=False):
+    colors = getcolorlist(bars) 
+    values = getlist(bars)
+
+    bar_chart = BarChart(
+        values,
+        bar_colors=colors,
+        y_range=[0, max(values) + 1, 5], 
+        y_length=5
+    )
+    bars_group = bar_chart.bars
+    bar_chart.bars.set_opacity(0)
+
+    self.play(Create(bar_chart), run_time=1)
+    bar_chart.bars.set_opacity(1)
+    self.play(DrawBorderThenFill(bars_group),run_time=5)
+
+    self.wait(3)
+    
+
+    for i in range(5):
+        bars_group = playsingleshuffle(self,bars_group,(5-i)/2)
+
+    if(explain):
+        self.wait(15)
+    else:
+        self.wait(3)
+    
+
+    sorted_bars = sorted(bars_group, key=lambda bar: bar.get_x())
+    sorted_group = VGroup(*sorted_bars)
+    self.remove(bars_group)
+    self.add(sorted_group)
+
+    swapped=True
+
+    iterations=0
+    reps=0
+    while(swapped):
+        reps+=1
+        if(explain):
+            reps=1
+        swapped=False
+        for i_bar in range(bars-reps):
+            iterations+=1
+            prevcol1=str(sorted_group[i_bar].get_color())
+            prevcol2=str(sorted_group[i_bar+1].get_color())
+            sorted_group[i_bar].set_color(GREEN)
+            sorted_group[i_bar+1].set_color(GREEN)
+            
+            waittime=func(iterations)
+
+            if(sorted_group[i_bar].height>sorted_group[i_bar+1].height):
+                self.wait(waittime/2)
+                sorted_group=swaptwo(self,sorted_group,i_bar,i_bar+1,waittime,prevcol1,prevcol2)
+                
+                swapped=True
+            else:
+                self.wait(waittime)
+                sorted_group[i_bar].set_color(prevcol1)
+                sorted_group[i_bar+1].set_color(prevcol2)
+
+    self.wait(8)
+
+    self.play(FadeOut(bar_chart))
+    self.clear()
+
+def intro(self):
+    main_text = Text("Sortierverfahren", font_size=100)
+    self.play(DrawBorderThenFill(main_text), run_time=5)
+    self.play(main_text.animate.shift(UP * 2))
+
+    sub_text = Text("Informatik GFS", font_size=50)
+    sub_text.next_to(main_text, DOWN, aligned_edge=LEFT)
+    
+    author_text = Text("Joel Dankert", font_size=40)
+    author_text.next_to(sub_text, DOWN, aligned_edge=LEFT)
+    self.play(Write(sub_text),Write(author_text), run_time=3)
+
+    
+    grade_text = Text("11", font_size=100)
+    grade_text.next_to(main_text, DOWN, aligned_edge=RIGHT)
+    self.play(Write(grade_text), run_time=3)
+    self.wait(5)
+    self.play(FadeOut(main_text,grade_text,author_text,sub_text))
+
+
 
 class MainScene(Scene):
     def construct(self):
-        bars = 10
-        colors = getcolorlist(bars) 
-        values = getlist(bars)
+        
+        intro(self)
+        self.wait(3)
 
-        bar_chart = BarChart(
-            values,
-            bar_colors=colors,
-            y_range=[0, max(values) + 1, 10], 
-            y_length=5
-        )
-        bars_group = bar_chart.bars
-        bar_chart.bars.set_opacity(0)
+        
 
-        self.play(Create(bar_chart), run_time=1)
-        bar_chart.bars.set_opacity(1)
-        self.play(DrawBorderThenFill(bars_group),run_time=5)
+        bubblesort(self,5,lambda x:max(5*0.90**x,0.02),True)
+        self.wait(3)
+        bubblesort(self,15,lambda x:3*0.94**x+0.05,False)
 
         self.wait(3)
-        
-
-        for i in range(5):
-            bars_group = playsingleshuffle(self,bars_group,(5-i)/2)
-
-        self.wait(3)
-        
-
-        sorted_bars = sorted(bars_group, key=lambda bar: bar.get_x())
-        sorted_group = VGroup(*sorted_bars)
-        self.remove(bars_group)
-        self.add(sorted_group)
-
-        swapped=True
-        while(swapped):
-            swapped=False
-            for i_bar in range(bars-1):
-                prevcol1=str(sorted_group[i_bar].get_color())
-                prevcol2=str(sorted_group[i_bar+1].get_color())
-                sorted_group[i_bar].set_color(GREEN)
-                sorted_group[i_bar+1].set_color(GREEN)
-                
 
 
-                if(sorted_group[i_bar].height>sorted_group[i_bar+1].height):
-                    sorted_group=swaptwo(self,sorted_group,i_bar,i_bar+1,0.2,prevcol1,prevcol2)
-                    
-                    swapped=True
-                else:
-                    self.wait(0.1)
-        
-        
-        self.wait(3)
